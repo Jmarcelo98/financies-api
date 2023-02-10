a58f9113ac9f281b7d3d9fb71ea773892c23eae5
João Marcelo <joaomarcelo588@gmail.com>
2023-02-10 17:15:51 -0300

    Created entity user

---
+++ src/main/java/com/financies/financiesapi/model/entities/User.java
@@ -0,0 +1,43 @@
+package com.financies.financiesapi.model.entities;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String name;
+
+	private String cpf;
+
+	private LocalDate dateCreation;
+
+	private LocalDate dateBirth;
+
+	@Lob
+	private Byte[] photo;
+
+}
